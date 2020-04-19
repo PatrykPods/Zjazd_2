613ece1952da67cb6886f32af9ec29409980dbd2
patrykpods <patryk.podsiadlo@gmail.com>
2020-04-19 11:36:23 +0200

    asda

--- zadanie_znaki.py
+++ zadanie_znaki.py
@@ -1,8 +1,23 @@
 def zliczanie(text, counter) :
+    znaki = dict()
+    znaki_2 = set()
+    for x in text :
+        if x in znaki :
+            znaki[x] = znaki[x] + 1
+        else :
+            znaki[x] = 1
 
-    for x in range(len(text)) :
+        if znaki[x] > counter :
+            znaki_2.add(x)
+
+    return znaki_2
+
+
+print(zliczanie("aaaaa bbbb ccccc", 4))
 
 
 
 def test_test_wiecej_niz_dla_pustego_napisu() :
-    assert wiecej_niz("", 0) == set()
+    assert zliczanie("", 0) == set()
+def test_wiecej_niz_dla_niepustego_napis() :
+    assert zliczanie("aaaaa bbbbb eeee ttttttt jjjjjjjjjjjj", 7) == {'t', 'j'}
